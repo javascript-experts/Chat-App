c3c5f18043617398c4a272a9861bee1e2815278c
Simran Kaur <simrankaur@Simrans-Mac-mini.local>
2019-07-16 17:22:05 +0530

    hello

--- route/userRoute.js
+++ route/userRoute.js
@@ -7,15 +7,23 @@ const path = require('path');
 const router = express.Router();
 
 router.route("/").get((req, res, next) => {
-  // res.setHeader("Content-Type", "application/json");
-  // res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
 
-  // connectdb.then(db => {
-  //   // let data = Users.find({ message: "Anonymous" });
-  //   Users.find({}).then(user => {
-  //     res.json(user);
-  //   });
-  // });
+  connectdb.then(db => {
+    // let data = Users.find({ message: "Anonymous" });
+    Users.find({}).then(user => {
+      res.json(user);
+    });
+
+    Users.create({
+      name: req.body.name
+      },
+      function (err, user) {
+          if (err) return res.status(500).json({"message": "There was a problem adding the information to the database."});
+          res.json(user);
+      });
+  });
 
   res.sendFile(path.resolve('./public/user-list.html'))
 });
